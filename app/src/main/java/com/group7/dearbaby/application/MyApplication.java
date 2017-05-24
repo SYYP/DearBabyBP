ec3785042aea9ec7877bf90d3cee5c80aae99db2
houhou1349010738 <1349010738@qq.com>
2017-05-24 17:17:53 +0800

    一次合并后的错误修改

--- app/src/main/java/com/group7/dearbaby/application/MyApplication.java
+++ app/src/main/java/com/group7/dearbaby/application/MyApplication.java
@@ -3,7 +3,13 @@ package com.group7.dearbaby.application;
 import android.app.Application;
 
 import com.blankj.utilcode.util.Utils;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareConfig;
 import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+import com.zhy.autolayout.config.AutoLayoutConifg;
+
+import cn.smssdk.SMSSDK;
 
 /**
  * @author holmes k
@@ -12,11 +18,21 @@ import com.uuzuche.lib_zxing.activity.ZXingLibrary;
 
 public class MyApplication extends Application {
 
+    {
+        PlatformConfig.setSinaWeibo("3383892678","fbf0b15e0e2c4d88f2688255f8a9d69e","http://sns.whalecloud.com");
+        PlatformConfig.setQQZone("1106030997","2wNCn7IxE3PIz7xF");
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Utils.init(this);
+        AutoLayoutConifg.getInstance().useDeviceSize();
+        UMShareAPI.get(this);
+        UMShareConfig config = new UMShareConfig();
+        config.isNeedAuthOnGetUserInfo(true);
+        UMShareAPI.get(getApplicationContext()).setShareConfig(config);
+        SMSSDK.initSDK(this, "1cfb207aa3b62", "c4715a08fbaa52917dc830b2254075da");
         Utils.init(this);
         ZXingLibrary.initDisplayOpinion(this);
     }
